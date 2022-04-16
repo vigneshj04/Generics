9279192f02cfd6ebc7a8ad7089e1a4296151ca99
vigneshj04 <vigneshjmax@gmail.com>
2022-04-16 20:43:28 +0530

    Added: To Find Maximum Of Given Three Numbers Using Generics

--- Generics.java
+++ Generics.java
@@ -3,5 +3,25 @@ import java.util.Scanner;
 
 public class Generics {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please Enter the 3 Numbers to check Maximum ");
+
+        int x= scan.nextInt();
+        int y= scan.nextInt();
+        int z= scan.nextInt();
+
+        maxOfNumber(x,y,z);
+    }
+
+    public static <T extends Integer> T maxOfNumber(T x, T y, T z) {
+        T max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        System.out.println(max);
+        return max;
     }
 }
