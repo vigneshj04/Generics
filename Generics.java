a0572dfa15a638aedcd9a475bf2257b492356b50
vigneshj04 <vigneshjmax@gmail.com>
2022-04-17 16:31:14 +0530

    Added: To Find Maximum Of Given Three Words

--- Generics.java
+++ Generics.java
@@ -5,24 +5,24 @@ import java.util.Scanner;
 public class Generics {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please Enter the 3 Numbers to check Maximum ");
+        System.out.println("Please Enter the 3 Words to Check Maximum ");
 
-        ArrayList<Float> floats = new ArrayList<>();
-        Float x = scan.nextFloat();
-        Float y = scan.nextFloat();
-        Float z = scan.nextFloat();
-        floats.add(x);
-        floats.add(y);
-        floats.add(z);
+        ArrayList<String> string = new ArrayList<>();
+        String x = scan.next();
+        String y = scan.next();
+        String z = scan.next();
+        string.add(x);
+        string.add(y);
+        string.add(z);
 
-        System.out.println(floats);
+        System.out.println(string);
         maxOfNumber(x,y,z);
     }
 
-    public static <T extends Float> T maxOfNumber(T x, T y, T z) {
+    public static <T extends String> T maxOfNumber(T x, T y, T z) {
         T max = x;
         if (y.compareTo(max) > 0) {
-            max = y;
+              max = y;
         }
         if (z.compareTo(max) > 0) {
             max = z;
